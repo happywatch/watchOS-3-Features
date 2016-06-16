aaea0c03be67624a1e3975e7553bbd6ef804c7de
Ben Morrow <ben@benmorrow.info>
2016-06-16 09:57:10 -0700

    Recreating project for Xcode 8, watchOS 3, Swift 3

---
+++ Features WatchKit Extension/FeatureInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  FeatureInterfaceController.swift
+//  Features
+//
+//  Created by temporary on 6/16/16.
+//  Copyright © 2016 happywatch. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class FeatureInterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: AnyObject?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Features/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Features
+//
+//  Created by temporary on 6/16/16.
+//  Copyright © 2016 happywatch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
